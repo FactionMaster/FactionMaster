b8920df04bbc3245d30d508eb78d5c4f17256fa9
ShockedPlot7560 <66992287+ShockedPlot7560@users.noreply.github.com>
2021-09-01 21:08:46 +0200

    Implements image

--- src/ShockedPlot7560/FactionMaster/Button/ViewHomes.php
+++ src/ShockedPlot7560/FactionMaster/Button/ViewHomes.php
@@ -32,6 +32,7 @@
 
 namespace ShockedPlot7560\FactionMaster\Button;
 
+use jojoe77777\FormAPI\SimpleForm;
 use ShockedPlot7560\FactionMaster\Route\HomeListPanel;
 use ShockedPlot7560\FactionMaster\Route\RouterFactory;
 use ShockedPlot7560\FactionMaster\Utils\Utils;
@@ -47,7 +48,10 @@ class ViewHomes extends Button {
             }, 
             function($Player) {
                 Utils::processMenu(RouterFactory::get(HomeListPanel::SLUG), $Player);
-            }
+            },
+            [],
+            "textures/img/house",
+            SimpleForm::IMAGE_TYPE_PATH
         );
     }
 
