add8644109a3616f395c1faa31d37ca6f7c4aacf
HugoLeBoss91 <55756021+HugoLeBoss91@users.noreply.github.com>
2022-03-05 18:25:00 +0100

    Update HomeEntity

--- src/Database/Entity/HomeEntity.php
+++ src/Database/Entity/HomeEntity.php
@@ -35,6 +35,7 @@ namespace ShockedPlot7560\FactionMaster\Database\Entity;
 use pocketmine\math\Vector3;
 use pocketmine\world\Position;
 use pocketmine\world\World;
+use pocketmine\entity\Location;
 use ShockedPlot7560\FactionMaster\FactionMaster as Main;
 use ShockedPlot7560\FactionMaster\Utils\Utils;
 
@@ -57,19 +58,19 @@ class HomeEntity extends EntityDatabase {
 	/**
 	 * DO NOT USE THIS CONSTANT
 	 * @see getX()
-	 * @var int
+	 * @var int|float
 	 */
 	public $x;
 	/**
 	 * DO NOT USE THIS CONSTANT
 	 * @see getY()
-	 * @var int
+	 * @var int|float
 	 */
 	public $y;
 	/**
 	 * DO NOT USE THIS CONSTANT
 	 * @see getZ()
-	 * @var int
+	 * @var int|float
 	 */
 	public $z;
 	/**
@@ -78,31 +79,59 @@ class HomeEntity extends EntityDatabase {
 	 * @var string
 	 */
 	public $world;
+	/**
+	 * DO NOT USE THIS CONSTANT
+	 * @see getYaw()
+	 * @var float
+	 */
+	public $yaw = 0.0;
+	/**
+	 * DO NOT USE THIS CONSTANT
+	 * @see getPitch()
+	 * @var float
+	 */
+	public $pitch = 0.0;
 
 	public function setName(string $name): void {
 		$this->name = $name;
 	}
 
-	public function setX(int $x): void {
+	public function setX(int|float $x): void {
 		$this->x = $x;
 	}
 
-	public function setY(int $y): void {
+	public function setY(int|float $y): void {
 		$this->y = $y;
 	}
 
-	public function setZ(int $z): void {
+	public function setZ(int|float $z): void {
 		$this->z = $z;
 	}
 
 	public function setWorldName(string $worldName): void {
 		$this->world = $worldName;
 	}
-
+	
+	public function setYaw(float $yaw): void {
+	  $this->yaw = $yaw;
+	}
+	
+	public function setPitch(float $pitch): void {
+	  $this->pitch = $pitch;
+	}
+	
 	public function setVector(Vector3 $vector): void {
 		$this->setX($vector->getX());
 		$this->setY($vector->getY());
 		$this->setX($vector->getX());
+		
+		if($vector instanceof Position) {
+		  $this->setWorldName($vector->getWorld()->getDisplayName());
+		  if($vector instanceof Location) {
+		    $this->setYaw($vector->getYaw());
+		    $this->setPitch($vector->getPitch());
+		  }
+		}
 	}
 
 	public function getName(): string {
@@ -121,7 +150,7 @@ class HomeEntity extends EntityDatabase {
 	}
 
 	public function toString(): string {
-		return Utils::homeToString($this->getX(), $this->getY(), $this->getZ(), $this->getLevelName());
+		return Utils::homeToString($this->getX(), $this->getY(), $this->getZ(), $this->getLevelName(), $this->getYaw(), $this->getPitch());
 	}
 
 	public function __toString(): string {
@@ -129,19 +158,27 @@ class HomeEntity extends EntityDatabase {
 	}
 
 	public function toArray(): array {
-		return Utils::homeToArray($this->getX(), $this->getY(), $this->getZ(), $this->getLevelName());
+		return Utils::homeToArray($this->getX(), $this->getY(), $this->getZ(), $this->getLevelName(), $this->getYaw(), $this->getPitch());
 	}
 
-	public function getX(): int {
+	public function getX(): int|float {
 		return $this->x;
 	}
 
-	public function getY(): int {
+	public function getY(): int|float {
 		return $this->y;
 	}
 
-	public function getZ(): int {
+	public function getZ(): int|float {
 		return $this->z;
+	}
+	
+	public function getYaw(): float {
+	  return $this->yaw;
+	}
+	
+	public function getPitch(): float {
+	  return $this->pitch;
 	}
 
 	public function getVector(): Vector3 {
@@ -150,5 +187,9 @@ class HomeEntity extends EntityDatabase {
 
 	public function getPosition(): Position {
 		return new Position($this->getX(), $this->getY(), $this->getZ(), $this->getLevel());
+	}
+	
+	public function getLocation(): Location {
+		return new Location($this->getX(), $this->getY(), $this->getZ(), $this->getLevel(), $this->getYaw(), $this->getPitch());
 	}
 }
