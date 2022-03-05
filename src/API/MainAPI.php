917beeff12939f1eddbaabfaa3333042be1d6333
HugoLeBoss91 <55756021+HugoLeBoss91@users.noreply.github.com>
2022-03-05 18:38:07 +0100

    Fixes

--- src/API/MainAPI.php
+++ src/API/MainAPI.php
@@ -945,13 +945,13 @@ class MainAPI {
 	public static function addHome(Player $player, string $factionName, string $name): void {
 		self::submitDatabaseTask(
 			new DatabaseTask(
-				"INSERT INTO " . HomeTable::TABLE_NAME . " (x, y, z, world, faction, name, server) VALUES (:x, :y, :z, :world, :pitch, :yaw, :faction, :name, :server)",
+				"INSERT INTO " . HomeTable::TABLE_NAME . " (x, y, z, world, yaw, pitch, faction, name, server) VALUES (:x, :y, :z, :world, :yaw, :pitch, :faction, :name, :server)",
 				[
 					"x" => $player->getPosition()->getX(),
 					"z" => $player->getPosition()->getZ(),
 					"y" => $player->getPosition()->getY(),
-					"pitch" => $player->getLocation()->getPitch(),
 					"yaw" => $player->getLocation()->getYaw(),
+					"pitch" => $player->getLocation()->getPitch(),
 					"world" => $player->getWorld()->getDisplayName(),
 					"faction" => $factionName,
 					"name" => $name,
