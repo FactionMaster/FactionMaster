af829566da1fec98c54f9387699b46a67976111a
HugoLeBoss91 <55756021+HugoLeBoss91@users.noreply.github.com>
2022-03-05 18:14:53 +0100

    Update FactionTpCommand

--- src/Command/Subcommand/HomeTpCommand.php
+++ src/Command/Subcommand/HomeTpCommand.php
@@ -65,8 +65,8 @@ class HomeTpCommand extends FactionSubCommand {
 		if (Utils::haveAccess($permissions, $userEntity, PermissionIds::PERMISSION_TP_FACTION_HOME)) {
 			$home = MainAPI::getFactionHome($userEntity->getFactionName(), $args["name"]);
 			if ($home !== null) {
-				$sender->teleport(new Vector3($home->getX(), $home->getY(), $home->getZ()));
-				(new FactionHomeTpEvent($sender, $userEntity->getFactionName(), $args['name'], $home))->call();
+				$sender->teleport($home->getVector(), $home->getYaw(), $home->getPitch());
+				(new FactionHomeTpEvent($sender, $userEntity->getFactionName(), $home))->call();
 				$sender->sendMessage(Utils::getText($sender->getName(), "SUCCESS_HOME_TELEPORT"));
 			} else {
 				$sender->sendMessage(Utils::getText($sender->getName(), "HOME_DONT_EXIST"));
